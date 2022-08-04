9dfe8a8caaa8dcf75213ddcabc187aac0dfecd84
Hehongyuanlove <1016823618@qq.com>
2022-08-04 06:24:48 +0000

    ✨ feat(前端综合): 用户搜索、@用户名

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,3 +1,17 @@
+import Model from 'flarum/common/Model';
+import User from 'flarum/common/models/User';
+
+import IgnoreSearchUser from './IgnoreSearchUser'
+import IgnoreMentions from "./IgnoreMentions"
+
+
 app.initializers.add('kater/ignore-user-extend', () => {
-  console.log('[kater/ignore-user-extend] Hello, forum!');
-});
+    console.log('[kater/ignore-user-extend] Hello, forum!');
+
+    User.prototype.ignore_all = Model.attribute('ignore_all');
+
+    IgnoreSearchUser()
+
+    IgnoreMentions()
+    
+}, -1);
